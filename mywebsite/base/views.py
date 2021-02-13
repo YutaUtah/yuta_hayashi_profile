17f61ff6515b0b6e58dbada7f93bfaf030872828
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2021-02-13 18:01:49 +0900

    created dog_pics function

--- mywebsite/base/views.py
+++ mywebsite/base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
 	return render(request, 'base/home.html')
+
+def dog_pics(request):
+	return render(request, 'base/other_templates/dog_page.html')
+
+def index(request):
+	return HttpResponse("Hello, world. You're at the polls index.")
